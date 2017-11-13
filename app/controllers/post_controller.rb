13be73e1464d0ace9160f0323577f9735b48a4bd
biniprc <biniprc@hotmail.com>
2017-11-13 02:45:45 +0000

    CRUD basic

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,52 @@
+class PostController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+  end
+
+  def create
+    @title = params[:title]
+    @content = params[:content]
+    
+    Post.create(
+      title: @title,
+      content: @content
+    )
+    redirect_to '/'
+  end
+
+  def show
+    @id = params[:id]
+    @post = Post.find(@id)
+  end
+  
+  def modify
+    @id = params[:id]
+    @post = Post.find(@id)
+  end
+  
+  def update
+    # 1번 글에 대해서 나는 수정을 할거야
+    # 1번 글을 찾는다.
+    @id = params[:id]
+    @post = Post.find(@id)
+    # 해당 글을 업데이트한다.
+    @post.update(
+      title: params[:title],
+      content: params[:content]
+    )
+    
+    redirect_to "/post/show/#{@id}"
+  end
+  
+  def destroy
+    @id = params[:id]
+    @post = Post.find(@id)
+    
+    @post.destroy
+    
+    redirect_to "/"
+  end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,23 @@
+class UserController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+    @email = params[:email]
+    @password = params[:password]
+    
+    User.create(
+      email: @email,
+      password: @password
+    )
+    
+    redirect_to '/'
+  end
+
+  def show
+  end
+end
